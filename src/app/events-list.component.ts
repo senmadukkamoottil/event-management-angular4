5d9a78e1f3af0e60fb3758ce8bd6d2f991aaad4e
Sen M <sentvla@gmail.com>
2018-04-03 23:05:59 -0700

    Todo sort function

--- src/app/events-list.component.ts
+++ src/app/events-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import { EventService } from './event.service';
 import { Router } from '@angular/router';
 
@@ -9,8 +9,9 @@ import { Router } from '@angular/router';
     th, td { font-size: 16px; padding:5px 10px;}
   `]
 })
-export class EventsListComponent {
+export class EventsListComponent implements OnChanges {
   events: any;
+  sortType: String = 'Descending';
   constructor(private eventService: EventService, private route: Router) {
     this.events = this.eventService.getEvents();
   }
@@ -18,5 +19,40 @@ export class EventsListComponent {
   editEvent(id) {
     this.route.navigate(['editevent', id]);
   }
+
+  ngOnChanges(): void {
+    console.log('here');
+  }
+
+  // Todo Sort function
+  sortEventList(sortOrder) {
+    if (this.sortType === 'Ascending' ) {
+      this.events.sort(this.sortFunction1);
+    } else {
+      this.events.sort(this.sortFunction2);
+    }
+    this.sortType = (sortOrder === 'Ascending' ? 'Descending' : 'Ascending');
+  }
+
+  sortFunction1(e1: any, e2: any): number {
+    if (e1.name > e2.name) {
+      return 1;
+    } else if ( e1.name === e2.name) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+
+sortFunction2(e1: any, e2: any): number {
+  if (e1.name < e2.name) {
+    return 1;
+  } else if ( e1.name === e2.name) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
 }
 
